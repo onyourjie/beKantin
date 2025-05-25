45dd589298c5522a2310eb32e86bb495f023b2ff
BoRrrr <robialwan8@gmail.com>
2025-05-25 22:29:10 +0700

    perbaikan cors

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
 
 app.use(
     cors({
-        origin: ["http://192.168.1.20:3000", "http://localhost:5173"],
+        origin: "*",
     })
 );
 app.use(express.json());
